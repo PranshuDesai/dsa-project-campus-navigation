42864b5c089f1f7161d3f688630d59fd8dd5b9dd
PranshuDesai <b24cs1057@iitj.ac.in>
2025-11-14 20:05:41 +0530

    Update graph_with_path.py

--- python/graph_with_path.py
+++ python/graph_with_path.py
@@ -1,1 +1,108 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+
+edges = [
+    ("B1", "G2", 3),
+    ("G2", "LHC1", 5),
+    ("G2", "LHC2", 15),
+    ("G2", "Veg Mess", 2),
+    ("G2", "NonVeg Mess", 4),
+    ("G2", "Sports Complex", 10),
+    ("G2", "Main Gate", 20),
+    ("G2", "Library", 10),
+
+    ("B1", "LHC1", 3),
+    ("B1", "LHC2", 18),
+    ("B1", "Veg Mess", 5),
+    ("B1", "NonVeg Mess", 2),
+    ("B1", "Sports Complex", 15),
+    ("B1", "Main Gate", 17),
+    ("B1", "Library", 7),
+
+    ("Main Gate", "Library", 13),
+    ("Library", "LHC1", 5),
+    ("LHC1", "LHC2", 12),
+    ("Library", "NonVeg Mess", 10),
+    ("Library", "Veg Mess", 13),
+
+    ("Veg Mess", "Sports Complex", 8),
+    ("NonVeg Mess", "Sports Complex", 11),
+    ("Veg Mess", "Main Gate", 22),
+    ("NonVeg Mess", "Main Gate", 19)
+]
+
+G.add_weighted_edges_from(edges)
+
+pos = {
+    "LHC2": (0, 5),
+    "G2": (0, 3),
+    "LHC1": (0, 1),
+    "Library": (0, -1),
+    "Main Gate": (0, -3),
+
+    "B1": (-3, 3),
+
+    "Veg Mess": (3, 4),
+    "NonVeg Mess": (3, 2),
+    "Sports Complex": (3, 0.5)
+}
+
+print("\nAvailable Locations:")
+for node in G.nodes():
+    print(" -", node)
+
+source = input("\nEnter source: ").strip()
+dest = input("Enter destination: ").strip()
+
+if source not in G or dest not in G:
+    print("\nInvalid location name! Please check spelling.")
+    exit()
+
+try:
+    path = nx.dijkstra_path(G, source, dest, weight='weight')
+    distance = nx.dijkstra_path_length(G, source, dest, weight='weight')
+except nx.NetworkXNoPath:
+    print("\nNo path found between these locations.")
+    exit()
+
+print("\nShortest Distance:", distance, "minutes")
+print("Path:", " -> ".join(path))
+
+edge_colors = []
+for u, v in G.edges():
+    if (u, v) in list(zip(path, path[1:])) or (v, u) in list(zip(path, path[1:])):
+        edge_colors.append("red")      
+    else:
+        edge_colors.append("black")   
+
+plt.figure(figsize=(10, 12))
+
+nx.draw_networkx_nodes(
+    G, pos,
+    node_color="#a4d7ff",
+    node_size=2500,
+    edgecolors="black",
+    linewidths=1.3
+)
+
+nx.draw_networkx_edges(
+    G, pos,
+    width=3,
+    edge_color=edge_colors
+)
+
+nx.draw_networkx_labels(G, pos, font_size=10, font_weight="bold")
+
+nx.draw_networkx_edge_labels(G, pos,
+    edge_labels=nx.get_edge_attributes(G, "weight"),
+    font_size=9
+)
+
+plt.title("IIT Jodhpur Campus Map with Shortest Path Highlighted", fontsize=16)
+plt.axis("off")
+plt.tight_layout()
+plt.show()
+
 
